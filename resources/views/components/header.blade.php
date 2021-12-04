e319fe552eb3a9f27a8e2a07449aae513a184fe0
Md Maruf Billah <maruf.fci@gmail.com>
2021-12-04 22:36:51 +0600

    Header design

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,12 +1,12 @@
 <div class="container-fluid">
-    <div class="row nav-row">
+    <div class="row main-nav">
         <div class="col-6">
             <div class="logo">
                 <img src="{{ URL::asset('image/logo.png') }}">
             </div>
         </div>
         <div class="col-6">
-            <button class="btn btn-danger">Account</button>
+            <button class="btn btn-danger account-button">Account</button>
         </div>
     </div>
 </div>
